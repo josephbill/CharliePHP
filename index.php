a87c9d1aae9eae8f3a10963b1caf1829477121c2
josephbill <josephbill00@gmail.com>
2021-02-22 13:59:13 +0300

    intro to PHP session code

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php //start tag
+
+//so to output in PHP
+echo "Hello world from PHP echo code";
+echo "<br>";
+echo "<h1>output of HTML</h1>";
+echo "<br>";
+print "Hello world from PHP print code";
+
+//close tag
+?> 
---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php
+//define a variable
+$username = "Joseph";
+$username;
+$_username;
+$stringVariable = "Billy";  //string variable 
+
+$username = 7;
+$username = false;
+
+$intVariable = 25; // integer variable 
+$booleanVariable = false; //boolean variable
+$floatvariable = 1.45; //float 
+$arrayVariable = array(25.0,30.0,45.45); //float array
+
+echo "My name is "." ". $username; 
+
+
+
+?> 
